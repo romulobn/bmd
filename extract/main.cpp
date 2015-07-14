a4384ef5618ac3fe28259bcda48512acfa430634
Rômulo Bourget Novas <romulo.bnovas@gmail.com>
2015-07-13 23:44:58 -0300

    Command line improvements.

--- extract/main.cpp
+++ extract/main.cpp
@@ -8,28 +8,27 @@
 
 int main(int argc, char *argv[])
 {
-    
     QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName("fiber-image-processor");
+    QCoreApplication::setApplicationName("morphometric-feature-extractor");
     QCoreApplication::setApplicationVersion("1.0");
     
     QCommandLineParser parser;
-    parser.setApplicationDescription("Myelinated fiber image processor");
+    parser.setApplicationDescription("Morphometric feature extractor");
     parser.addHelpOption();
     parser.addVersionOption();
     
-    QCommandLineOption targetDirectoryOption(QStringList() << "t" << "target-file", QCoreApplication::translate("main", "Save all extracted morphometric measures into <target-file>."), QCoreApplication::translate("main", "target-directory"));
-    parser.addOption(targetDirectoryOption);
+    QCommandLineOption outputFileOption(QStringList() << "o" << "output-file", QCoreApplication::translate("main", "Save all extracted morphometric measures into <output-file>."), QCoreApplication::translate("main", "output-file"));
+    parser.addOption(outputFileOption);
     
     QCommandLineOption sourceDirectoryOption(QStringList() << "s" << "source-directory", QCoreApplication::translate("main", "Extract the morphometric measure of all images from the <source-directory>."), QCoreApplication::translate("main", "source-directory"));
     parser.addOption(sourceDirectoryOption);
     
     QCommandLineOption scaleValueOption(QStringList() << "c" << "scale", QCoreApplication::translate("main", "Extract the morphometric measures using the the <scale>. The default value is assumed to be 0.123636 nm/px."), QCoreApplication::translate("main", "scale"));
-    parser.addOption(sourceDirectoryOption);
+    parser.addOption(scaleValueOption);
     
     parser.process(app);
     
-    if( !parser.isSet(targetDirectoryOption) || !parser.isSet(sourceDirectoryOption) )
+    if( !parser.isSet(outputFileOption) || !parser.isSet(sourceDirectoryOption) )
     {
         parser.showHelp();
         return -1;
@@ -42,8 +41,8 @@ int main(int argc, char *argv[])
         scale = scaleString.toFloat();
     }
     
-    QString targetDirectory = parser.value(targetDirectoryOption);
+    QString outputFile = parser.value(outputFileOption);
     QString sourceDirectory = parser.value(sourceDirectoryOption);
 
-    ImageExtractor().measureImages(sourceDirectory, targetDirectory, QStringList() << "jpg" << "jpeg" << "tif" << "tiff" << "bmp", scale);
+    ImageExtractor().measureImages(sourceDirectory, outputFile, QStringList() << "jpg" << "jpeg" << "tif" << "tiff" << "bmp", scale);
 }
