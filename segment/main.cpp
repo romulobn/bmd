1093afbb498b219fa2fc541b30c94dbb5deb729b
Rômulo Bourget Novas <romulo.bnovas@gmail.com>
2015-07-13 22:51:22 -0300

    Command line improvements.

--- segment/main.cpp
+++ segment/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
 #include <QDebug>
 #include <QDir>
 
@@ -6,41 +7,59 @@
 
 int main(int argc, char *argv[])
 {
-    ImageProcessor imp;
 
-    QStringList args;
-
-    for(int i = 0; i < argc; i++)
-        args.append(argv[i]);
-
-    if (args.size() < 5)
-    {
-        qDebug() << "Error not enough arguments!";
-        exit(1);
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("fiber-image-processor");
+    QCoreApplication::setApplicationVersion("1.0");
+    
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Myelinated fiber image processor");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    
+    QCommandLineOption targetDirectoryOption(QStringList() << "t" << "target-directory", QCoreApplication::translate("main", "Save all processed files into <target-directory>."), QCoreApplication::translate("main", "target-directory"));
+    parser.addOption(targetDirectoryOption);
+    
+    QCommandLineOption sourceDirectoryOption(QStringList() << "s" << "source-directory", QCoreApplication::translate("main", "Process all images from the <source-directory>."), QCoreApplication::translate("main", "source-directory"));
+    parser.addOption(sourceDirectoryOption);
+    
+    QCommandLineOption nerveTypeOption(QStringList() << "n" << "nerve-type", QCoreApplication::translate("main", "Process as the <nerve-type>."), QCoreApplication::translate("main", "nerve-type"));
+    parser.addOption(nerveTypeOption);
+    
+    QCommandLineOption pipelineTypeOption(QStringList() << "p" << "pipeline-type", QCoreApplication::translate("main", "Process using the <pipeline-type> pipeline."), QCoreApplication::translate("main", "pipeline-type"));
+    parser.addOption(pipelineTypeOption);
+    
+    parser.process(app);
+    
+    if(!parser.isSet(targetDirectoryOption) || !parser.isSet(sourceDirectoryOption) || !parser.isSet(nerveTypeOption) || !parser.isSet(pipelineTypeOption)) {
+        parser.showHelp();
+        return -1;
     }
+    
+    QString targetDirectory = parser.value(targetDirectoryOption);
+    QString sourceDirectory = parser.value(sourceDirectoryOption);
+    QString nerveType = parser.value(nerveTypeOption);
+    QString pipelineType = parser.value(pipelineTypeOption);
+    
+    int nerveEnum, pipelineEnum;
+    
+    if(nerveType == "lar")
+        nerveEnum = ImageProcessor::Laryngeal;
+    if(nerveType == "ves")
+        nerveEnum = ImageProcessor::Vestibulochoclear;
+    if(nerveType == "sur")
+        nerveEnum = ImageProcessor::Sural;
+    if(nerveType == "vag")
+        nerveEnum = ImageProcessor::Vagus;
+    if(nerveType == "fre")
+        nerveEnum = ImageProcessor::Frenic;
+    
+    if(pipelineType == "color")
+        pipelineEnum = ImageProcessor::Color;
+    if(pipelineType == "morph")
+        pipelineEnum = ImageProcessor::Morph;
 
-    QStringList filters;
-    int nerve, pipeline;
-    filters << "jpg" << "jpeg" << "tif" << "tiff" << "bmp";
-
-    qDebug() << args[3];
-    qDebug() << args[4];
-
-    if(args[3] == "lar")
-        nerve = ImageProcessor::Laryngeal;
-    if(args[3] == "ves")
-        nerve = ImageProcessor::Vestibulochoclear;
-    if(args[3] == "sur")
-        nerve = ImageProcessor::Sural;
-    if(args[3] == "vag")
-        nerve = ImageProcessor::Vagus;
-    if(args[3] == "fre")
-        nerve = ImageProcessor::Frenic;
-
-    if(args[4] == "color")
-        pipeline = ImageProcessor::Color;
-    if(args[4] == "morph")
-        pipeline = ImageProcessor::Morph;
-
-    imp.processImages(args[1], args[2], filters, nerve, pipeline);
+    ImageProcessor().processImages(sourceDirectory, targetDirectory, QStringList() << "jpg" << "jpeg" << "tif" << "tiff" << "bmp", nerveEnum, pipelineEnum);
+    
+    return 0;
 }
